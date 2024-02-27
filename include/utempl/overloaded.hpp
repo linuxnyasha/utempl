a970526e4bd31dab272188107990704969d529db
sha512sum <linuxnyasha@tutanota.com>
2024-02-27 18:40:00 +0000

    Add overloaded

---
+++ include/utempl/overloaded.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <utility>
+
+namespace utempl {
+template <typename... Fs>
+inline constexpr auto Overloaded(Fs&&... fs) {
+  struct Overloaded : public std::remove_cvref_t<Fs>... {
+    using Fs::operator()...;
+  };
+  return Overloaded{std::forward<Fs>(fs)...};
+};
+
+} // namespace utempl
