9c3b4e6ec08d12d023dd65d72cb24202d83cf354
sha512sum <linuxnyasha@tutanota.com>
2024-03-01 17:14:05 +0000

    Remove unnacessary in Tuple example

--- examples/src/tuple.cpp
+++ examples/src/tuple.cpp
@@ -5,12 +5,12 @@
 
 auto main() -> int {
   using utempl::literals::operator""_c;
-  constexpr utempl::Tuple tuple = utempl::Tuple{42, 3.141500, "Hello World"};
+  constexpr utempl::Tuple tuple = {42, 3.141500, "Hello World"};
   utempl::Tuple<int> tuple2{{}}; 
   std::ignore = tuple2;
   std::cout << utempl::kTupleSize<decltype(tuple)> << std::endl; // Get tuple size
   std::cout << tuple[0_c] << std::endl; // Get element using [] with literal
-  auto newTuple = utempl::Transform(tuple, utempl::Overloaded(
+  auto newTuple = Transform(tuple, utempl::Overloaded(
     [](auto arg){
       return std::to_string(arg);
     },
